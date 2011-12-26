a7136ceb018c093f1106c364ddce5a89a698fe03
Greg Leppert <greg@formasfunction.com>
2011-12-26 11:50:04 -0500

    Fixing injection

--- chrome/content.js
+++ chrome/content.js
@@ -1,1 +1,14 @@
-//alert('hit');
+var domain = '0.0.0.0:3000';
+
+// if the user came from a reading link
+if(document.referrer.indexOf(domain) > -1){
+  var head = document.getElementsByTagName('head')[0],
+      vars = document.createElement('script'),
+      script = document.createElement('script');
+  
+  vars.appendChild(document.createTextNode('var reading = {platform: "chrome"};'));
+  script.src="http://"+domain+"/assets/bookmarklet/loader.js";
+
+  head.appendChild(vars);
+  head.appendChild(script);
+}
