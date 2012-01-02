680fd175adf35128079b6ce42f23dda1c4f3b53f
Greg Leppert <greg@formasfunction.com>
2012-01-01 20:21:12 -0500

    Working on context menu

--- chrome/background.js
+++ chrome/background.js
@@ -1,17 +1,20 @@
-var pageSearch = function(info, tab) {
-    // Send the selected image to TinEye
+var pageSearch = function(info, tab){
     sort_order = sortOrder();
     open_url("http://" + SERVER + "/search/?pluginver=chrome-" + CHROME_VERSION +  sort_order + "&url=" + encodeURIComponent(info.srcUrl));
 };
 
-var page_menu = chrome.contextMenus.create({"title": "Post to Reading",
-                                            "contexts": ["all"],
-                                            "onclick": pageSearch});
+var contexts = ["page","link","image","video","audio"];
+for(var i = 0; i < contexts.length; i++){
+  var context = contexts[i],
+      title = "Post " + context + " to Reading";
+  chrome.contextMenus.create({"title": title, "contexts": [context], "onclick": pageSearch});
+}
 
 chrome.browserAction.onClicked.addListener(function(tab){
   chrome.tabs.executeScript(null, {file:"action.js"});
 });
 
+// This is currently handled in the content script manifest option
 // chrome.tabs.onUpdated.addListener(function(tabId , info){
   // if(info.status == "complete"){
     // chrome.tabs.executeScript(null, {file:"content.js"});
