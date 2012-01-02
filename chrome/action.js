7c3cf5033e4de83909c1132b915d0dfb8f1d9d69
Greg Leppert <greg@formasfunction.com>
2012-01-01 21:18:42 -0500

    Message passing is working

--- chrome/action.js
+++ chrome/action.js
@@ -1,11 +1,19 @@
 var DOMAIN = '0.0.0.0:3000';
 
-var head = document.getElementsByTagName('head')[0],
-    vars = document.createElement('script'),
-    script = document.createElement('script');
+// var head = document.getElementsByTagName('head')[0],
+//     vars = document.createElement('script'),
+//     script = document.createElement('script');
 
-vars.appendChild(document.createTextNode('var reading = {platform: "chrome"};'));
-script.src="http://"+DOMAIN+"/assets/bookmarklet/loader.js";
+// vars.appendChild(document.createTextNode('var reading = {platform: "chrome"};'));
+// script.src="http://"+DOMAIN+"/assets/bookmarklet/loader.js";
 
-head.appendChild(vars);
-head.appendChild(script);
+// head.appendChild(vars);
+// head.appendChild(script);
+
+
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse){
+    console.log(request.url);
+    sendResponse({});
+  }
+);
--- chrome/background.js
+++ chrome/background.js
@@ -1,12 +1,15 @@
 var self = this;
-    post = function(url){
+    post = function(url, tab){
       console.log(url);
+      chrome.tabs.executeScript(tab.id, {file: "action.js"}, function(){
+        chrome.tabs.sendRequest(tab.id, {url: url});
+      });
     },
-    post_page = function(info, tab){ post(info.pageUrl); },
-    post_link = function(info, tab){ post(info.linkUrl); },
-    post_image= function(info, tab){ post(info.srcUrl); },
-    post_video= function(info, tab){ post(info.srcUrl); },
-    post_audio= function(info, tab){ post(info.srcUrl); };
+    post_page = function(info, tab){ post(info.pageUrl, tab); },
+    post_link = function(info, tab){ post(info.linkUrl, tab); },
+    post_image= function(info, tab){ post(info.srcUrl, tab); },
+    post_video= function(info, tab){ post(info.srcUrl, tab); },
+    post_audio= function(info, tab){ post(info.srcUrl, tab); };
 
 var contexts = ["page","link","image","video","audio"];
 for(var i = 0; i < contexts.length; i++){
@@ -16,7 +19,7 @@ for(var i = 0; i < contexts.length; i++){
 }
 
 chrome.browserAction.onClicked.addListener(function(tab){
-  chrome.tabs.executeScript(null, {file:"action.js"});
+  chrome.tabs.executeScript(tab.id, {file: "action.js"});
 });
 
 // This is currently handled in the content script manifest option
