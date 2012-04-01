68826ba4348073d80644d0752849b1acc312c60e
Greg Leppert <greg@formasfunction.com>
2012-04-01 18:58:18 -0400

    Working on safari message passing

--- reading.safariextension/background.js
+++ reading.safariextension/background.js
@@ -1,9 +1,21 @@
 // dispatch
-safari.application.addEventListener("command", function(event){
+var handle_command = function(event){
   console.log('command', event);
   safari.application.activeBrowserWindow.activeTab.page.dispatchMessage(event.command, {url:'http://example.com'});
-}, false);
+};
 
 // validate
-safari.application.addEventListener("validate", function(event){
-}, false);
+var handle_validate = function(event){
+};
+
+var handle_cm = function(event){
+  console.log('open cm', event);
+  if (event.userInfo === "IMG") {
+    event.contextMenu.appendContextMenuItem("enlarge", "Post image to Reading");
+  }
+};
+
+// attach listeners
+safari.application.addEventListener("command", handle_command, false);
+safari.application.addEventListener("validate", handle_validate, false);
+safari.application.addEventListener("contextmenu", handle_cm, false);
