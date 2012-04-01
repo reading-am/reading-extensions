345adb0e5a09315234630d017d6de34cdb2d21a2
Greg Leppert <greg@formasfunction.com>
2012-04-01 03:07:00 -0400

    Making headway on safari

--- reading.safariextension/background.js
+++ reading.safariextension/background.js
@@ -1,2 +1,9 @@
-safari.application.addEventListener("command", function(event){ console.log('command', event); }, false);
-safari.application.addEventListener("validate", function(event){ console.log('validate', event); }, false);
+// dispatch
+safari.application.addEventListener("command", function(event){
+  console.log('command', event);
+  safari.application.activeBrowserWindow.activeTab.page.dispatchMessage(event.command, {url:'http://example.com'});
+}, false);
+
+// validate
+safari.application.addEventListener("validate", function(event){
+}, false);
