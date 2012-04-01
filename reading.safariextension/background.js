f07d8ef827db162fdd9cbda68f8dc9c3e832ec57
Greg Leppert <greg@formasfunction.com>
2012-04-01 02:31:57 -0400

    Working on the safari background.js

---
+++ reading.safariextension/background.js
@@ -0,0 +1,2 @@
+safari.application.addEventListener("command", function(event){ console.log('command', event); }, false);
+safari.application.addEventListener("validate", function(event){ console.log('validate', event); }, false);
