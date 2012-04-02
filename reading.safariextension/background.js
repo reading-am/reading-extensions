78c478442422eec586a944fa975dcaaca5a0d128
Greg Leppert <greg@formasfunction.com>
2012-04-01 20:31:14 -0400

    Cleaning up the background script to make it more chrome like

--- reading.safariextension/background.js
+++ reading.safariextension/background.js
@@ -1,30 +1,45 @@
-// dispatch
+//-------//
+// Setup //
+//-------//
 var handle_command = function(event){
   console.log('command', event);
-
-  if(event.command == 'page'){
-    var message = {
-      url: safari.application.activeBrowserWindow.activeTab.url,
-      title: safari.application.activeBrowserWindow.activeTab.title
-    };
+  if(event.target instanceof SafariExtensionToolbarItem){
+    main_button();
   } else {
-    var message = click_info[event.command];
-  };
-
-  safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('submit', message);
+    click_info[event.command].onclick();
+  }
 };
 
-// validate
-var handle_validate = function(event){
+//-------------//
+// Track Usage //
+//-------------//
+var track = function(button, event){
+  if(!event) event = 'clicked';
+  _gaq.push(['_trackEvent', button, event]);
+};
+
+//-------------------//
+// Submit to Reading //
+//-------------------//
+var submit = function(type, via, url, title){
+  track(via+'_'+type);
+  safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('submit', {url: url, title: title});
+};
+
+//--------------------//
+// Main Plugin Button //
+//--------------------//
+var main_button = function(){
+  submit('page', 'browserAction', safari.application.activeBrowserWindow.activeTab.url, safari.application.activeBrowserWindow.activeTab.title);
 };
 
 //---------------//
 // Context Menus //
 //---------------//
 var contexts = [
-      {type: "page"},
-      {type: "link",  selector:"a"},
-      {type: "image", selector:"img"}
+      {type: "page",  func: function(){ submit('page', 'contextMenu', safari.application.activeBrowserWindow.activeTab.url, safari.application.activeBrowserWindow.activeTab.title); }},
+      {type: "link",  selector:"a",   func: function(){ submit('link',  'contextMenu', click_info.link.url); }},
+      {type: "image", selector:"img", func: function(){ submit('image', 'contextMenu', click_info.image.url); }}
     ],
     click_info = {};
 var handle_cm = function(event){
@@ -32,18 +47,17 @@ var handle_cm = function(event){
   for(var i = 0; i < event.userInfo.length; i++){
     for(var j = 0; j < contexts.length; j++){
       if(event.userInfo[i].name == contexts[j].selector){
-        click_info[contexts[j].type] = {url: event.userInfo[i].url};
-        event.contextMenu.appendContextMenuItem(contexts[j].type, "* Post "+contexts[j].type+" to Reading");
+        click_info[contexts[j].type] = {url: event.userInfo[i].url, onclick: contexts[j].func};
+        event.contextMenu.appendContextMenuItem(contexts[j].type, "Post "+contexts[j].type+" to Reading");
       }
     }
   }
   // if none of the selectors match, show "read page" context
   if(!event.contextMenu.contextMenuItems.length){
-    event.contextMenu.appendContextMenuItem(contexts[0].type, "* Post "+contexts[0].type+" to Reading");
+    event.contextMenu.appendContextMenuItem(contexts[0].type, "Post "+contexts[0].type+" to Reading");
   }
 };
 
 // attach listeners
 safari.application.addEventListener("command", handle_command, false);
-safari.application.addEventListener("validate", handle_validate, false);
 safari.application.addEventListener("contextmenu", handle_cm, false);
