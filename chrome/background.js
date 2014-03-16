56ffd2c3f142db04324a7967293a653ebbc7cadd
Greg Leppert <greg@leppert.me>
2014-03-16 10:29:47 -0400

    Add CSP bypass code and bump version to 1.1.4

--- chrome/background.js
+++ chrome/background.js
@@ -1,7 +1,9 @@
 //-------//
 // Setup //
 //-------//
-var self = this;
+var ROOT_DOMAIN = 'reading.am';
+// uncomment for local testing
+// ROOT_DOMAIN = 'reading.dev:3000';
 
 //-------------//
 // Track Usage //
@@ -42,4 +44,57 @@ for(var i = 0; i < contexts.length; i++){
     "contexts": [contexts[i].type],
     "onclick": contexts[i].func
   });
+}
+
+//------------//
+// CSP Bypass //
+//------------//
+// https://www.planbox.com/blog/development/coding/bypassing-githubs-content-security-policy-chrome-extension.html
+chrome.webRequest.onHeadersReceived.addListener(function(details) {
+  for (i = 0; i < details.responseHeaders.length; i++) {
+
+    if (isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
+      var csp = details.responseHeaders[i].value,
+          defsrcs = ['http://*.'+ROOT_DOMAIN, 'https://*.'+ROOT_DOMAIN],
+          // via: https://developer.mozilla.org/en-US/docs/Security/CSP/CSP_policy_directives
+          directives = {
+            'default-src': defsrcs,
+            'script-src': [
+              "'unsafe-eval'",
+              'http://*.'+ROOT_DOMAIN,
+              'https://*.'+ROOT_DOMAIN
+            ],
+            'object-src': defsrcs,
+            'img-src':    defsrcs,
+            'media-src':  defsrcs,
+            'frame-src':  defsrcs,
+            'font-src':   defsrcs,
+            'connect-src': [
+              'http://*.'+ROOT_DOMAIN,
+              'https://*.'+ROOT_DOMAIN,
+              "ws://*.pusherapp.com",
+              "wss://*.pusherapp.com",
+              // SockJS fallback endpoints
+              "http://*.pusher.com",
+              "https://*.pusher.com"
+            ],
+            'style-src':  defsrcs,
+            'report-uri': defsrcs
+          };
+
+      for (var key in directives) csp = csp.replace(key, key+" "+directives[key].join(" "));
+      details.responseHeaders[i].value = csp;
+    }
+  }
+
+  return { // Return the new HTTP header
+    responseHeaders: details.responseHeaders
+  };
+}, {
+  urls: ["http://*/*", "https://*/*"],
+  types: ["main_frame"]
+}, ["blocking", "responseHeaders"]);
+
+function isCSPHeader(headerName) {
+  return (headerName == 'CONTENT-SECURITY-POLICY') || (headerName == 'X-WEBKIT-CSP');
 }
